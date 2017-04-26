42b44cdca1bc6cadb51920a2809e1f1de7a1dbfe
Frances Lei Vargas <francesleivargas@gmail.com>
2017-04-26 15:58:14 +0800

    initial implementation of fetch

--- Instruction.java
+++ Instruction.java
@@ -1,15 +1,73 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
 public class Instruction{
-	String operation;
-	String op1;
-	String op2;
+	private String operation;
+	private String op1;
+	private String op2;
+	private Integer val1;
+	private Integer val2;
 
 	public Instruction(String operation, String op1, String op2){
 		this.operation = operation;
 		this.op1 = op1;
 		this.op2 = op2;
+		this.val1 = 0;
+		this.val2 = 0;
 	}
 
 	public void printShit(){
 		System.out.println(this.operation+" "+this.op1+" "+this.op2);
+	}
+
+	public String getOperation(){
+		return this.operation;
+	}
+
+	public String getOperand1(){
+		return this.op1;
+	}
+
+	public String getOperand2(){
+		return this.op2;
+	}
+
+	public Integer getValue1(){
+		return this.val1;
+	}
+
+	public Integer getValue2(){
+		return this.val2;
+	}
+
+	public void fetch(HashMap<String,Integer> registers)
+	{
+		if (isRegister(this.op1))
+		{
+			this.val1 = registers.get(this.op1);
+		}
+		else
+		{
+			this.val1 = Integer.parseInt(this.op1);
+		}
+
+		if (isRegister(this.op2))
+		{
+			this.val2 = registers.get(this.op2);
+		}
+		else
+		{
+			this.val2 = Integer.parseInt(this.op2);
+		}
+
+		System.out.println(this.op1+" "+this.op2);
+		System.out.println("Val 1: " + this.val1 + " Val 2: " + this.val2);
+	}
+
+	private boolean isRegister(String operand)
+	{
+		Pattern REGISTERPATTERN = Pattern.compile("r[1-9]|r1[0-9]|r2[0-9]|r3[0-2]");
+		Matcher m = REGISTERPATTERN.matcher(operand);
+		return m.matches();
 	}
 }
--- PC.java
+++ PC.java
@@ -1,26 +1,55 @@
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 
 public class PC{
+
 	public static void main(String []args){
+		
+		HashMap<String,Integer> registers = new HashMap<String,Integer>();
+		registers.put("r1",0);
+		registers.put("r2",0);
+		registers.put("r3",0);
+		registers.put("r4",0);
+		registers.put("r5",0);
+		registers.put("r6",0);
+		registers.put("r7",0);
+		registers.put("r8",0);
+		registers.put("r9",0);
+		registers.put("r10",0);
+		registers.put("r11",0);
+		registers.put("r12",0);
+		registers.put("r13",0);
+		registers.put("r14",0);
+		registers.put("r15",0);
+		registers.put("r16",0);
+		registers.put("r17",0);
+		registers.put("r18",0);
+		registers.put("r19",0);
+		registers.put("r20",0);
+		registers.put("r21",0);
+		registers.put("r22",0);
+		registers.put("r23",0);
+		registers.put("r24",0);
+		registers.put("r25",0);
+		registers.put("r26",0);
+		registers.put("r27",0);
+		registers.put("r28",0);
+		registers.put("r29",0);
+		registers.put("r30",0);
+		registers.put("r31",0);
+		registers.put("r32",0);
+
 		int linecount=0;
-		ArrayList<Instruction> instructions = new ArrayList<Instruction>();
-		ArrayList<String> possibleOperations = new ArrayList<String>();
-		possibleOperations.add("add");
-		possibleOperations.add("sub");
-		possibleOperations.add("load");
-		possibleOperations.add("cmp");
 		String currentLine;
-		final registerPattern = new Pattern("r[0-32]");
-	
+		Verifier verifier = new Verifier();
+		ArrayList<Instruction> instructions = new ArrayList<Instruction>();
 
 		try{
 			BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 			while((currentLine = br.readLine())!=null){
 				linecount++;
-				currentLine = currentLine.trim();
-				currentLine = currentLine.toLower();
-				currentLine = currentLine.replace("\n", "");
+				currentLine = currentLine.replaceAll("\\s"," ");
+				currentLine = currentLine.toLowerCase();
 				if(currentLine.equals("")) continue;
 				String[] temp = currentLine.split("");
 				String acc = "";
@@ -35,19 +64,28 @@ public class PC{
 					}
 				}
 				if(!acc.equals("")) arr.add(acc);
-				System.out.println();
 				if(arr.size()==3){
 					Instruction instr = new Instruction(arr.get(0),arr.get(1), arr.get(2));
-					instructions.add(instr);
+					if(verifier.checkValidity(instr)) 
+					{
+						instructions.add(instr);
+						instr.fetch(registers);
+					}
+					else{
+						System.out.print(arr.toString());
+						System.out.println(" is an invalid instruction set!");
+					}
 				}else{
 					System.out.println("Error");
 				}
 			}
 
+			for(Instruction i : instructions){
+				i.printShit();
+			}
+
 		}catch(Exception e){
 
 		}
-	}	
-
-
+	}
 }
